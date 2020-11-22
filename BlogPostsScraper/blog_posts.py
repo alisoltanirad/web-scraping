7dfcb3ddd45184e299904c88f7637966b4875173
alisoltanirad <soltaniradali@gmail.com>
2020-11-22 15:57:18 +0330

    Add blog posts scraper

---
+++ BlogPostsScraper/blog_posts.py
@@ -0,0 +1,15 @@
+# https://github.com/alisoltanirad/Web-Scraping.git
+# Data Source: https://dev.to
+# Dependencies: BeautifulSoup
+import requests
+import re
+from decimal import Decimal
+from bs4 import BeautifulSoup
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
