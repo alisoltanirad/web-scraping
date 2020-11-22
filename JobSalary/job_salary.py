5ba339e7968c6c590b387f5ed3672ca9c7d81823
alisoltanirad <soltaniradali@gmail.com>
2020-11-22 15:39:30 +0330

    Add job salary project

---
+++ JobSalary/job_salary.py
@@ -0,0 +1,48 @@
+# https://github.com/alisoltanirad/Web-Scraping.git
+# Data Source: https://neuvoo.ca
+# Dependencies: BeautifulSoup
+import requests
+import re
+from decimal import Decimal
+from bs4 import BeautifulSoup
+
+
+def main():
+    show_job_salaries(get_job_salaries())
+
+
+def show_job_salaries(dataset):
+    for data in sorted(dataset, key=dataset.get, reverse=True):
+        print('{job:>35}: {salary:>8,}'.format(job=data, salary=dataset[data]))
+
+
+def get_job_salaries():
+    salaries = {}
+    for job in get_jobs():
+        try:
+            salaries[get_title(job)] = get_salary(job)
+        except:
+            continue
+    return salaries
+
+
+def get_title(job):
+    return job.find(class_='truncate').text.strip()
+
+
+def get_salary(job):
+    salary = job.find(class_='card--infoList--li--perYear timeBased')
+    return Decimal(re.sub(r'[^\d.]', '', salary.text.strip()))
+
+
+def get_jobs():
+    return get_page().find_all(class_='card--infoList--li')
+
+
+def get_page():
+    page = requests.get('https://neuvoo.ca/salary/')
+    return BeautifulSoup(page.content, 'html.parser')
+
+
+if __name__ == '__main__':
+    main()
