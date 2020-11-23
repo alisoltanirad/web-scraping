a0a0301366fb229ad04b97931f1e3a03f2ee59e2
alisoltanirad <soltaniradali@gmail.com>
2020-11-23 20:20:31 +0330

    Add get_posts function

--- BlogPostsScraper/blog_posts.py
+++ BlogPostsScraper/blog_posts.py
@@ -11,8 +11,16 @@ def main():
     pass
 
 
+def get_posts():
+    post_links = {}
+    post_list = get_page().find_all(class_='crayons-story__hidden-navigation-link')
+    for post in post_list:
+        post_links[post.text] = post.get('href')
+    return post_links
+
+
 def get_page():
-    page = requests.get('https://dev.to/t/react')
+    page = requests.get('https://dev.to')
     return BeautifulSoup(page.content, 'html.parser')
 
 
