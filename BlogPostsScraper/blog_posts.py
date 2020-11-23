05b5f034e3e60f25c45a8d5ad64efd05b050b274
alisoltanirad <soltaniradali@gmail.com>
2020-11-23 20:41:36 +0330

    Refactor blog posts module

--- BlogPostsScraper/blog_posts.py
+++ BlogPostsScraper/blog_posts.py
@@ -2,38 +2,41 @@
 # Data Source: https://dev.to
 # Dependencies: BeautifulSoup
 import requests
-import re
-from decimal import Decimal
 from bs4 import BeautifulSoup
+
+class DevBlog():
+    url = 'https://dev.to'
+
+    def __init__(self):
+        self._page = BeautifulSoup(
+            requests.get('https://dev.to').content, 'html.parser'
+        )
+        self.posts = self._get_posts()
+
+
+    def post_titles(self):
+        for post in self.posts:
+            yield (post['title'])
+
+
+    def _get_posts(self):
+        posts = []
+        post_elements = self._page.find_all(
+            class_='crayons-story__hidden-navigation-link'
+        )
+        for post_element in post_elements:
+            post = {
+                'title': post_element.text,
+                'link': post_element.get('href'),
+            }
+            posts.append(post)
+        return posts
+
 
 
 def main():
-    show_post_titles()
-
-
-def show_post_titles():
-    posts = get_posts()
-    for post in posts:
-        print(post['title'])
-
-
-def get_posts():
-    posts = []
-    post_elements = get_page().find_all(
-        class_='crayons-story__hidden-navigation-link'
-    )
-    for post_element in post_elements:
-        post = {
-            'title': post_element.text,
-            'link': post_element.get('href'),
-        }
-        posts.append(post)
-    return posts
-
-
-def get_page():
-    page = requests.get('https://dev.to')
-    return BeautifulSoup(page.content, 'html.parser')
+    for title in DevBlog().post_titles():
+        print(title)
 
 
 if __name__ == '__main__':
