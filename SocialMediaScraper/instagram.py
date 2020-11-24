c686dfab3be1201593ce725b780c5e1ca67106b0
alisoltanirad <soltaniradali@gmail.com>
2020-11-24 22:38:22 +0330

    Add get profile data method

--- SocialMediaScraper/instagram.py
+++ SocialMediaScraper/instagram.py
@@ -2,6 +2,8 @@
 # Data Source: https://instagram.com
 # Dependencies: BeautifulSoup
 import requests
+import re
+import json
 from bs4 import BeautifulSoup
 
 class InstagramUser():
@@ -10,14 +12,22 @@ class InstagramUser():
     def __init__(self, username):
         self.username = username
         self.url = ''.join([self.start_url, self.username])
-        self.page = BeautifulSoup(
+        self._page = BeautifulSoup(
             requests.get(self.url).content, 'html.parser'
         )
+        self._data = self._get_profile_data()
+
+    def _get_profile_data(self):
+        shared_data = self._page.find(
+            'script', text=re.compile('window\._sharedData')
+        ).string.partition('=')[-1].strip(' ;')
+
+        return json.loads(shared_data)
+
 
 
 def main():
     coder = InstagramUser('coder24.7')
-    print(coder.page)
 
 
 if __name__ == '__main__':
