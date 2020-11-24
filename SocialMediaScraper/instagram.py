d61b9f6d9cf80dbc64a2ff53501b08c32a6cf6eb
alisoltanirad <soltaniradali@gmail.com>
2020-11-24 22:09:31 +0330

    Add instagram user class

--- SocialMediaScraper/instagram.py
+++ SocialMediaScraper/instagram.py
@@ -1,17 +1,23 @@
 # https://github.com/alisoltanirad/Web-Scraping.git
 # Data Source: https://instagram.com
-# Dependencies:
+# Dependencies: BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-class Instagram():
-    url = 'https://instagram.com'
+class InstagramUser():
+    start_url = 'https://instagram.com/'
 
-    def __init__(self):
-        pass
+    def __init__(self, username):
+        self.username = username
+        self.url = ''.join([self.start_url, self.username])
+        self.page = BeautifulSoup(
+            requests.get(self.url).content, 'html.parser'
+        )
 
 
 def main():
-    pass
+    coder = InstagramUser('coder24.7')
+    print(coder.page)
 
 
 if __name__ == '__main__':
