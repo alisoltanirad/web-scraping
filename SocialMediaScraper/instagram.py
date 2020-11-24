4005aa196c43b0cea2ee3f59dcb6cb3e6b2fea27
alisoltanirad <soltaniradali@gmail.com>
2020-11-24 21:42:02 +0330

    Add instagram scraper

---
+++ SocialMediaScraper/instagram.py
@@ -0,0 +1,18 @@
+# https://github.com/alisoltanirad/Web-Scraping.git
+# Data Source: https://instagram.com
+# Dependencies:
+import requests
+
+class Instagram():
+    url = 'https://instagram.com'
+
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
