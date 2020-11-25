412c191f83d6371f10b933fc8297e8648913e1ca
alisoltanirad <soltaniradali@gmail.com>
2020-11-25 16:09:34 +0330

    Update instagram user scraper

--- SocialMediaScraper/instagram.py
+++ SocialMediaScraper/instagram.py
@@ -10,24 +10,41 @@ class InstagramUser():
     start_url = 'https://instagram.com/'
 
     def __init__(self, username):
-        self.username = username
-        self.url = ''.join([self.start_url, self.username])
+        self._username = username
+        self._url = ''.join([self.start_url, self._username])
         self._page = BeautifulSoup(
-            requests.get(self.url).content, 'html.parser'
+            requests.get(self._url).content, 'html.parser'
         )
-        self._data = self._get_profile_data()
+        self._profile = self._get_profile_data()
+        self.data = {
+            'Name': self._profile['full_name'],
+            'Username': self._username,
+            'Page-URL': self._url,
+            'Profile-Picture-URL': self._profile['profile_pic_url_hd'],
+            'Biography': self._profile['biography'],
+            'Website': self._profile['external_url'],
+            'Followers': self._profile['edge_followed_by']['count'],
+            'Following': self._profile['edge_follow']['count'],
+            'Is-Private': self._profile['is_private'],
+            'Is_Verified': self._profile['is_verified'],
+            'Is-Business-Account': self._profile['is_business_account'],
+            'Page-Category': self._profile['category_enum'],
+            'Is-Joined-Recently': self._profile['is_joined_recently'],
+        }
 
     def _get_profile_data(self):
         shared_data = self._page.find(
             'script', text=re.compile('window\._sharedData')
         ).string.partition('=')[-1].strip(' ;')
 
-        return json.loads(shared_data)
+        return json.loads(shared_data)['entry_data']['ProfilePage'][0]['graphql']['user']
 
 
 
 def main():
-    coder = InstagramUser('coder24.7')
+    user = InstagramUser('coder24.7')
+    for key, value in user.data.items():
+        print('{key:>25}:  {value}'.format(key=key, value=value))
 
 
 if __name__ == '__main__':
