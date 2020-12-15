23c1ec654ee8deead8938988cbd4227d59cd7cbd
alisoltanirad <soltaniradali@gmail.com>
2020-12-15 18:07:04 +0330

    Update sets scraper

--- ShopScraper/brickset.py
+++ ShopScraper/brickset.py
@@ -1,15 +1,49 @@
 # Data Source: https://brickset.com/
-# Dependencies: ScrPy
+# Dependencies: BeautifulSoup
 import requests
-import scrapy
+from bs4 import BeautifulSoup
+
+class Brickset:
+    start_url = 'https://brickset.com/'
+
+    def __init__(self):
+        self.sets = self._get_sets()
+
+    def _get_sets(self):
+        sets_url = ''.join([self.start_url, 'sets/year-2020/'])
+        sets_pages = [
+            BeautifulSoup(requests.get(
+                ''.join([sets_url, 'page-', str(page)])
+            ).content, 'html.parser')
+            for page in range(1, 35)
+        ]
+
+        set_divs = list()
+        for page in sets_pages:
+            set_divs += page.find_all(class_='set')
+
+        sets = list()
+
+        for div in set_divs:
+            try:
+                sets.append({
+                    'Title': div.find('h1').text,
+                    'Price': div.find(
+                        'dt', text='RRP'
+                    ).findNext('dd').text.split('|')[0].strip(),
+                    'Pieces': div.find('dt', text='Pieces').findNext('dd').text,
+                    'Picture-URL': div.find('a').get('href'),
+                })
+            except AttributeError:
+                pass
+
+        return sets
 
 
-class BricksetSpider(scrapy.Spider):
-    name = 'brickset_spider'
-    start_urls = ['https://brickset.com/sets/year-2020']
-
-    def parse(self, response):
-        for item in response.css('.set'):
-            yield {
-                'Name': item.css('h1 :: text').extract_first(),
-            }
+if __name__ == '__main__':
+    brickset = Brickset()
+    print(len(brickset.sets))
+    for set in brickset.sets:
+        for key, value in set.items():
+            print('{key}: {value}'.format(key=key, value=value))
+        print()
