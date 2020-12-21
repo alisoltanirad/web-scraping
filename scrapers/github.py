8aef1636d29d7ed0ae008a53a73e070cb9b01449
alisoltanirad <soltaniradali@gmail.com>
2020-12-21 18:32:36 +0330

    Add repositories info

--- scrapers/github.py
+++ scrapers/github.py
@@ -15,33 +15,40 @@ class GithubUser:
             requests.get(self.url).content, 'html.parser'
         )
 
-    def get_profile_info(self):
-        info = {
+    def profile_info(self):
+        return {
+
             'Username': self.username,
+
             'Name': self._page.find(
                 'span', itemprop='name'
             ).text,
+
             'Bio': self._page.find(
                 'div', class_='user-profile-bio'
             ).text,
+
             'Followers': self._page.find(
                 'a', href='/alisoltanirad?tab=followers'
             ).find('span').text,
+
             'Following': self._page.find(
                 'a', href='/alisoltanirad?tab=following'
             ).find('span').text,
-        }
-        return info
 
-    def get_activity_info(self):
+            'URL': self.url,
+
+        }
+
+    def activity_info(self):
         timeline_items = [
             re.sub(r'\s+|\n', ' ', item.find('summary').text.strip())
             for item in self._page.find_all(
                 'div', class_='TimelineItem-body'
             )
         ]
-        print(len(timeline_items))
-        info = {
+
+        return {
             'Last-Year': re.sub(
                 r'\s+|\n',
                 ' ',
@@ -49,18 +56,75 @@ class GithubUser:
                     'div', class_='js-yearly-contributions'
                 ).find('h2').text.strip()
             ),
+            
             'Last-Month': ', '.join(timeline_items)
         }
-        return info
+
+    def repos_info(self):
+        page = self._get_tab('repositories')
+
+        repos = list()
+        for repo in page.find_all('li', class_='public'):
+            repos.append({
+
+                'Title': repo.find(
+                    'a', itemprop='name codeRepository'
+                ).text.strip(),
+
+                'Description': repo.find(
+                    'p', itemprop='description'
+                ).text.strip() if repo.find(
+                    'p', itemprop='description'
+                ).text.strip() is not None else 'None',
+
+                'Language': repo.find(
+                    'span', itemprop='programmingLanguage'
+                ).text.strip() if repo.find(
+                    'span', itemprop='programmingLanguage'
+                ) is not None else 'None',
+                
+                'Updated': repo.find(
+                    'relative-time'
+                ).text.strip(),
+
+                'Tags': ', '.join(
+                    tag.text.strip() for tag in repo.find_all(
+                        'a', class_='topic-tag'
+                    )
+                ) if repo.find_all('a', class_='topic-tag') != [] else 'None',
+
+                'URL': ''.join([
+                    self.start_url,
+                    repo.find(
+                    'a', itemprop='name codeRepository'
+                    ).get('href')[1:]
+                ]),
+
+            })
+
+        return repos
+
+    def _get_tab(self, name):
+        url = ''.join([self.url, '?tab=', name])
+        return BeautifulSoup(
+            requests.get(url).content, 'html.parser'
+        )
+
 
 
 if __name__ == '__main__':
     user = GithubUser('alisoltanirad')
 
-    print('\nProfile:\n')
-    for key, value in user.get_profile_info().items():
+    print('\n\tProfile:\n')
+    for key, value in user.profile_info().items():
         print('{key}: {value}'.format(key=key, value=value))
 
-    print('\nActivity:\n')
-    for key, value in user.get_activity_info().items():
+    print('\n\tActivity:\n')
+    for key, value in user.activity_info().items():
         print('{key}: {value}'.format(key=key, value=value))
+
+    print('\n\tRepositories:\n')
+    for repo in user.repos_info():
+        for key, value in repo.items():
+            print('{key}: {value}'.format(key=key, value=value))
+        print()
